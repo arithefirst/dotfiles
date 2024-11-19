48ac3215ba3f571b7d3293af777ee1ebf1528a6f
April Hall <ari@arithefirst.com>
2024-11-19 15:36:13 -0500

    Setup telescope

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,3 +3,16 @@ require("config.lazy")
 
 -- Make catppuccin default colorscheme
 vim.cmd.colorscheme "catppuccin-mocha"
+
+-- Set tabwidth to 2
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.bo.softtabstop = 2
+
+-- Telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
