a0770662ea597e7159dab073b66f7b0940764bcd
April Hall <ari@arithefirst.com>
2024-11-21 09:54:26 -0500

    Show hidden files by default in neotree

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -8,5 +8,14 @@ return {
 	},
 	config = function()
 		vim.keymap.set("n", "<leader>e", ":Neotree<CR>", { desc = "Neotree" })
+		require("neo-tree").setup({
+			filesystem = {
+				filtered_items = {
+					visible = true,
+					hide_dotfiles = false,
+					hide_gitignored = true,
+				},
+			},
+		})
 	end,
 }
