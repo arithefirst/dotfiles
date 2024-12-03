d839f3be3f25dbca508b7720edafd9c7984f425f
April Hall <ari@arithefirst.com>
2024-12-03 16:26:49 -0500

    Show line numbers

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -8,6 +8,9 @@ vim.opt.termguicolors = true
 -- Map <leader> to space
 vim.g.mapleader = " "
 
+-- Enable line numbers
+vim.cmd("set number")
+
 -- Set tabwidth to 2
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
